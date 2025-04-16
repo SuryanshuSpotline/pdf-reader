8288c35aaeb7752dd155eb38ec8ae2f8d3942645
Suryanshu Patnaik <suryanshu.patnaik@spotline.com>
2025-04-16 19:09:10 +0530

    Test

--- Server/index.js
+++ Server/index.js
@@ -3,7 +3,7 @@ import multer from "multer";
 import path from "path";
 import fs from "fs";
 import cors from "cors";
-import extractFontsFromPDF from "./extractFonts.js";  // Import the font extraction function
+import { extractFontsFromPDF } from './extractFonts.js';  // Import the font extraction function
 import dotenv from "dotenv";
 
 dotenv.config();
