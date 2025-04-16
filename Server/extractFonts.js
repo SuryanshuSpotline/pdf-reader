eef463d3c1d90d36197c0262d56f2b1df35ae708
Suryanshu Patnaik <suryanshu.patnaik@spotline.com>
2025-04-16 16:54:25 +0530

    Testing

--- Server/extractFonts.js
+++ Server/extractFonts.js
@@ -1,15 +1,16 @@
-require('dotenv').config();
-const axios = require('axios');
-const PDFServicesSdk = require('@adobe/pdfservices-node-sdk');
-const fs = require('fs');
-const path = require('path');
-const { getAdobeAccessToken } = require('./getAdobeAccessToken');
+import dotenv from 'dotenv';
+import axios from 'axios';
+import PDFServicesSdk from '@adobe/pdfservices-node-sdk';
+import fs from 'fs';
+import path from 'path';
+import { getAdobeAccessToken } from './getAdobeAccessToken.js';
+import AdmZip from 'adm-zip';
+
+dotenv.config();
 
 async function extractFontsFromPDF(pdfFilePath) {
-  // Get the Adobe Access Token (OAuth)
   const adobeAccessToken = await getAdobeAccessToken();
 
-  // Adobe PDF Services SDK credentials using OAuth token
   const credentials = PDFServicesSdk.Credentials
     .serviceAccountCredentialsBuilder()
     .fromJSON({
@@ -20,22 +21,25 @@ async function extractFontsFromPDF(pdfFilePath) {
   const executionContext = PDFServicesSdk.ExecutionContext.create(credentials);
   const extractPDFOperation = PDFServicesSdk.ExtractPDF.Operation.createNew();
 
-  const input = PDFServicesSdk.FileRef.createFromLocalFile(pdfFilePath, PDFServicesSdk.ExtractPDF.SupportedSourceFormat.pdf);
+  const input = PDFServicesSdk.FileRef.createFromLocalFile(
+    pdfFilePath,
+    PDFServicesSdk.ExtractPDF.SupportedSourceFormat.pdf
+  );
   extractPDFOperation.setInput(input);
 
   const options = new PDFServicesSdk.ExtractPDF.options.ExtractPDFOptions.Builder()
-    .addElementsToExtract(PDFServicesSdk.ExtractPDF.options.ExtractElementType.TEXT, PDFServicesSdk.ExtractPDF.options.ExtractElementType.FONT)
+    .addElementsToExtract(
+      PDFServicesSdk.ExtractPDF.options.ExtractElementType.TEXT,
+      PDFServicesSdk.ExtractPDF.options.ExtractElementType.FONT
+    )
     .build();
 
   extractPDFOperation.setOptions(options);
 
   const result = await extractPDFOperation.execute(executionContext);
-  const outputPath = path.join(__dirname, 'output', 'result.zip');
-
+  const outputPath = path.join(path.dirname(pdfFilePath), 'result.zip');
   await result.saveAsFile(outputPath);
 
-  // Unzip and parse fonts
-  const AdmZip = require('adm-zip');
   const zip = new AdmZip(outputPath);
   const jsonEntry = zip.getEntry('structuredData.json');
   const structuredData = JSON.parse(jsonEntry.getData().toString('utf8'));
@@ -50,4 +54,4 @@ async function extractFontsFromPDF(pdfFilePath) {
   return Array.from(fonts);
 }
 
-module.exports = { extractFontsFromPDF };
+export default extractFontsFromPDF;
--- Server/getAdobeAccessToken.js
+++ Server/getAdobeAccessToken.js
@@ -1,22 +1,22 @@
-require('dotenv').config();
-const axios = require('axios');
+import dotenv from 'dotenv';
+import axios from 'axios';
 
-async function getAdobeAccessToken() {
+dotenv.config();
+
+export async function getAdobeAccessToken() {
   const tokenEndpoint = 'https://api.adobe.io/auth/oauth/v2/token';
 
   const data = new URLSearchParams({
     client_id: process.env.ADOBE_CLIENT_ID,
     client_secret: process.env.ADOBE_CLIENT_SECRET,
-    grant_type: 'client_credentials',  // or 'authorization_code' if using OAuth 2.0 Authorization Code Flow
+    grant_type: 'client_credentials',  // Update this if you're using a different flow
   });
 
   try {
     const response = await axios.post(tokenEndpoint, data);
     return response.data.access_token;
   } catch (error) {
-    console.error('Error obtaining Adobe access token:', error);
+    console.error('Error obtaining Adobe access token:', error.response?.data || error.message);
     throw error;
   }
 }
-
-module.exports = { getAdobeAccessToken };
